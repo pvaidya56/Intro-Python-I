cf601cb9c4f3062f307f8d5070782ed78478de19
Priya <pvaidya56@gmail.com>
2020-06-02 20:09:30 -0700

    completed

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,9 +11,9 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-print('x is %d, y is %f, z is "%s" '% (x, y, z))
+print('x is %d, y is %0.2f, z is "%s" '% (x, y, z))
 
 # Use the 'format' string method to print the same thing
-print("x is {}, y is {}, z is {}".format(x, y, z))
+print("x is {0:2d}, y is {1:6.2f}, z is {2}".format(x, y, z))
 # Finally, print the same thing using an f-string
-print(f"x is {x}, y is {y}, z is {z}")
+print(f"x is {x}, y is {y:.2f}, z is {z}")
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -26,7 +26,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = [a[i].upper() for i in range(len(a))]
+y = [i.upper() for i in a]
 
 print(y)
 
@@ -37,6 +37,6 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 
-y = [i for i in range(len(x)) if (int(x[i]) % 2) != 0]
+y = [i for i in x if (int(i) % 2) == 0]
 
 print(y)
